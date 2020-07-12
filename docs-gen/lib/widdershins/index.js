1fd967dee5c324647d710847d3fa54da0c5a254b
Nuno Dâmaso <nuno.s.damaso@gmail.com>
2020-07-12 18:37:42 +0000

    update

--- docs-gen/lib/widdershins/index.js
+++ docs-gen/lib/widdershins/index.js
@@ -1,23 +1,24 @@
 'use strict'
 const converter = require('widdershins')
-let options = {} // defaults shown
-options.codeSamples = true
-options.httpsnippet = false
-// options.language_tabs = [];
-// options.language_clients = [];
-// options.loadedFrom = sourceUrl; // only needed if input document is relative
-// options.user_templates = './user_templates';
-options.templateCallback = function (templateName, stage, data) { return data }
-options.theme = 'darkula'
-options.search = true
-options.sample = true // set false by --raw
-options.discovery = false
-options.includes = []
-options.shallowSchemas = false
-options.tocSummary = false
-options.headings = 2
-options.yaml = false
-// options.resolve = false;
-// options.source = sourceUrl; // if resolve is true, must be set to full path or URL of the input document
+let options = {
+  codeSamples: true,
+  httpsnippet: false,
+  templateCallback: function (templateName, stage, data) { return data },
+  theme: 'darkula',
+  search: true,
+  sample: true, // set false by --raw
+  discovery: false,
+  includes: [],
+  shallowSchemas: false,
+  tocSummary: false,
+  headings: 2,
+  yaml: false
+  // options.language_tabs        : [];
+  // options.language_clients     : [];
+  // options.loadedFrom           : sourceUrl; // only needed if input document is relative
+  // options.user_templates       : './user_templates';
+  // options.resolve              : false;
+  // options.source               : sourceUrl; // if resolve is true, must be set to full path or URL of the input document
+}
 
 module.exports = apiObj => converter.convert(apiObj, options)
