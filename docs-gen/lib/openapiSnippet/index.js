02dcaa146c8275f13c7b2665ee7116816c055374
Nuno Dâmaso <nuno.s.damaso@gmail.com>
2020-07-13 10:59:53 +0000

    deal endpoint; postman

--- docs-gen/lib/openapiSnippet/index.js
+++ docs-gen/lib/openapiSnippet/index.js
@@ -1,10 +1,13 @@
+'use strict'
+
 const OpenAPISnippet = require('openapi-snippet')
-const targets = ['node_request', 'shell_curl', 'shell_httpie', 'python_python3', 'c_libcurl', 'java_unirest', 'javascript_xhr']
+const TARGETS = ['node_request', 'shell_curl', 'shell_httpie', 'python_python3', 'c_libcurl', 'java_unirest', 'javascript_xhr']
+
 module.exports = openApi => {
   try {
     for (var path in openApi.paths) {
       for (var method in openApi.paths[path]) {
-        var generatedCode = OpenAPISnippet.getEndpointSnippets(openApi, path, method, targets)
+        var generatedCode = OpenAPISnippet.getEndpointSnippets(openApi, path, method, TARGETS)
         openApi.paths[path][method]['x-code-samples'] = []
         for (var snippetIdx in generatedCode.snippets) {
           var snippet = generatedCode.snippets[snippetIdx]
