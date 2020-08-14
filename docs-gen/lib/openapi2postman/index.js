8d6005daa045667422f2240f6ad52a6b804fb211
Nuno Dâmaso <nuno.s.damaso@gmail.com>
2020-08-14 22:20:40 +0000

    cleaned old docgen; api updates

--- docs-gen/lib/openapi2postman/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const convert = require('util').promisify(require('openapi-to-postmanv2').convert)
-
-module.exports = spec => convert({ type: 'json', data: spec })
-  .then(({result, output, reason}) => (result && output[0].data) || Promise.reject(reason))
